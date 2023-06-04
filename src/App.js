5c1fd7bd6076750cc15274ad50bca91d0a1b65e9
Arturo Larios <arallariosqu@ittepic.edu.mx>
2023-06-03 20:30:37 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import { useEffect, useRef } from 'react';
 import './App.css';
 
+import videoSrc from './assets/video-test-2.mp4';
+
+const FPS = 30;
+let now;
+let then = Date.now();
+const INTERVAL = 1000 / FPS;
+let delta;
+
 function App() {
+
+  const video = useRef(null);
+  const canvas = useRef(null);
+  const animation = useRef(null);
+
+  useEffect(() => {
+    if (video.current && canvas.current) {
+      video.current.addEventListener('play', () => initCanvas());
+      video.current.addEventListener('pause', () => cancelAnimationFrame(animation.current));
+    }
+  }, [video, canvas]);
+
+  const initCanvas = () => {
+    const ctx = canvas.current.getContext('2d');
+    const { width, height } = video.current.getBoundingClientRect();
+    const positionWidth = (window.innerWidth / 2) - (width / 2);
+    const positionHeight = (window.innerHeight / 2) - (height / 2);
+
+    draw({
+      ctx,
+      width,
+      height,
+      positionWidth,
+      positionHeight
+    });
+  }
+
+  const draw = ({ ctx, width, height, positionWidth, positionHeight }) => {
+    animation.current = requestAnimationFrame(() => draw({
+      ctx,
+      width,
+      height,
+      positionWidth,
+      positionHeight
+    }));
+
+    now = Date.now();
+    delta = now - then;
+
+    if (delta > INTERVAL) {
+      then = now - (delta % INTERVAL);
+      ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+      ctx.filter = 'blur(100px)';
+      ctx.drawImage(video.current, positionWidth, positionHeight, width, height);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="theater-container">
+      <div className='theater'>
+        <canvas width={ window.innerWidth } height={ window.innerHeight } ref={ canvas } />
+      </div>
+      <video controls src={ videoSrc } loop autoPlay muted ref={ video } />
     </div>
   );
 }
